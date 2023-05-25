4f7a954f73b1c1afe2a0e5c0324ea5851f4a6239
Mateus Wouters <mateus.090603@bento.local>
2023-05-25 09:07:11 -0300

    Initial commit

---
+++ resources/views/teste-view.blade.php
@@ -0,0 +1,8 @@
+
+@extends('includes.base')
+@section('title', 'titulo joia')
+@section('content')
+<p> esté é o arquivo que criei  👀🧨</p>
+<p> ☆*: .｡. o(≧▽≦)o .｡.:*☆☆*: .｡. o(≧▽≦)o .｡.:*☆</p>
+<p> a variavel é {{$valor_da_controler}}</p>
+@endsection
