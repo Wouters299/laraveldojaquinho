1160caae6ff5ceabd2d624325d606dd3ec3cca6a
Mateus Wouters <mateus.090603@bento.local>
2023-05-25 10:11:56 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/teste/{algo?}',function ($algo = null){
-    return "O teste funciona 👌 {$algo}";
-});
-
-Route::get('/teste-view/{param?}', function ($param = null) {
-    return view('teste-view',
-    [
-        'valor_da_controler' => $param]);
-});
+Route::get('/produtos', [ProdutosController::class, 'index']);
